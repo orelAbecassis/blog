702f4fa7f1c24e77738975917dec64fdb7f03a1f
orelabecassis <orelabk6.oa.oa@gmail.com>
2023-03-20 14:27:07 +0100

    page de connexion

--- migrations/Version20230320094049.php
+++ migrations/Version20230320094049.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230320085919 extends AbstractMigration
+final class Version20230320094049 extends AbstractMigration
 {
     public function getDescription(): string
     {
---
+++ src/Form/ProduitsType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produits;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProduitsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom_produit')
+            ->add('prix')
+            ->add('legende')
+            ->add('description')
+            ->add('image')
+            ->add('id_categ')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Produits::class,
+        ]);
+    }
+}
